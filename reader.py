329e41013724e7d0ff3f65622499c24f09c875a5
ItisNoMatter <702sunnylife@gmail.com>
2022-06-21 23:13:43 +0900

    とりあえずやってみた

--- reader.py
+++ reader.py
@@ -1,1 +1,7 @@
-import music21
+import music21 as m21
+mf = m21.midi.MidiFile()
+mf.open('midifiles/bach_846.mid')
+mf.read()
+for event in mf.tracks[3].events:
+    if type(event) == m21.midi.MidiEvent:
+        print("pitch:"+str(event.pitch)+"   time:"+str(event.time))
